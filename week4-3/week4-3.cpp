bf81efe6182e16d8f86f3b0207ca31e2f7d5eeb9
blackkitten1 <blackkitten010@gmail.com>
2020-09-05 16:09:51 +0700

    .

--- week4-3/week4-3.cpp
+++ week4-3/week4-3.cpp
@@ -1,10 +1,12 @@
 #include<stdio.h>
 
-int max(int x, int y) {
-	if (x > y)
+int max(int x, int y,int z) {
+	if (x >= y and x >= z)
 		return x;
-	else
+	else if (y >= x and y >= z)
 		return y;
+	else
+		return z;
 }
 
 int main() {
@@ -14,7 +16,7 @@ int main() {
 		scanf_s("%d %d %d %d %d", &x[i][0], &x[i][1], &x[i][2], &x[i][3], &x[i][4]);
 		sum[i] = x[i][0] + x[i][1] + x[i][2] + x[i][3] + x[i][4];
 	}
-	printf("%d",max(max(max(sum[0],sum[1]),max(sum[2],sum[3])),sum[4]));
+	printf("%d",max(max(sum[0],sum[1],sum[2]),sum[3],sum[4]));
 	return 0;
 }
 
